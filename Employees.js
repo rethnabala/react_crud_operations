6dd814bae9b8dfa898fbb62f88f60550a6bf73c3
Rethna Bala <139537096+rethnabala@users.noreply.github.com>
2024-10-16 13:24:37 +0530

    Add files via upload

---
+++ Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+{
+    id:"1",
+    Name:"barath",
+    Age:"12"
+},{
+    id:"2",
+    Name:"bala",
+    Age:"23"
+},
+{
+    id:"3",
+    Name:"shan",
+    Age:"23"
+}
+]
+export default Employees;
